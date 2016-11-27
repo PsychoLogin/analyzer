282a52d2ada3163c16c4820e688fa6d39d1765ac
Othmar Kesseli <othmar.kesseli@outlook.com>
2016-11-27 20:09:26 +0100

    Added JUnit dependency.

---
+++ src/test/java/ch/bfh/projekt1/psyloginanalyzer/test/dl4j/Dl4jLoginAnalyzerTest.java
@@ -0,0 +1,14 @@
+package ch.bfh.projekt1.psyloginanalyzer.test.dl4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by othma on 27.11.2016.
+ */
+public class Dl4jLoginAnalyzerTest {
+    @Test
+    public void testAlarm() {
+        Assert.fail();
+    }
+}
---
+++ src/test/java/ch/bfh/projekt1/psyloginanalyzer/test/suite/AllTests.java
@@ -0,0 +1,13 @@
+package ch.bfh.projekt1.psyloginanalyzer.test.suite;
+
+import ch.bfh.projekt1.psyloginanalyzer.test.dl4j.Dl4jLoginAnalyzerTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by othma on 27.11.2016.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ Dl4jLoginAnalyzerTest.class })
+public class AllTests {
+}
