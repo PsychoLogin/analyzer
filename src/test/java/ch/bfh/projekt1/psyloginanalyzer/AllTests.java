ba73e5ebfc04aa7d0c092c681d673ae84c12e0a9
Othmar Kesseli <othmar.kesseli@outlook.com>
2016-12-10 17:39:29 +0100

    merged testsuite changes

--- src/test/java/ch/bfh/projekt1/psyloginanalyzer/AllTests.java
+++ src/test/java/ch/bfh/projekt1/psyloginanalyzer/AllTests.java
@@ -1,7 +1,7 @@
 package ch.bfh.projekt1.psyloginanalyzer;
 
+import ch.bfh.projekt1.psyloginanalyzer.analyzer.StaticDataAnalyzerTest;
 import ch.bfh.projekt1.psyloginanalyzer.analyzer.UserBehaviorAnalyserTest;
-import ch.bfh.projekt1.psyloginanalyzer.csv.CsvConverterTest;
 import ch.bfh.projekt1.psyloginanalyzer.dl4j.Dl4jLoginAnalyzerTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,6 +10,6 @@ import org.junit.runners.Suite;
  * Created by othma on 27.11.2016.
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({CsvConverterTest.class, Dl4jLoginAnalyzerTest.class, UserBehaviorAnalyserTest.class})
+@Suite.SuiteClasses({Dl4jLoginAnalyzerTest.class, StaticDataAnalyzerTest.class, UserBehaviorAnalyserTest.class})
 public class AllTests {
 }
